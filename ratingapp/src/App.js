7efdf8f58ab032450574ba98ddb78c0d380db51d
Freddy Fabian <99642629+Freddy444@users.noreply.github.com>
2023-11-08 04:20:01 -0500

    Updated  App.js to correct routings

--- ratingapp/src/App.js
+++ ratingapp/src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from "react";
+
 import './App.css';
+
+import HomeV from './components/homeview';
+import CreateV from './views/createview';
+import ReadV from './views/readview';
+import UpdateV from './views/updateview';
+import DeleteV from './views/deleteview';
+import LoginV from './views/loginview';
+import RegisterV from './views/registerview';
+
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // setting different routing parth for different files 
+    <BrowserRouter>
+        <Routes>
+              <Route path="/" element={<HomeV />} />
+              <Route path="/register" element={<RegisterV />} />
+              <Route path="/login" element={<LoginV />} />
+              <Route path="/create" element={<CreateV />} />
+              <Route path="/read" element={<ReadV />} />
+              <Route path="/update/:id" element={<UpdateV onClose={() =>{}} />} />
+              <Route path="/delete" element={<DeleteV />} />
+        </Routes>
+    </BrowserRouter>
   );
 }
-
 export default App;
