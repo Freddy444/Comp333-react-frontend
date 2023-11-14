9ccbcea46a1d000646a91a5e8ff5c40134b5d71e
Freddy Fabian <99642629+Freddy444@users.noreply.github.com>
2023-11-14 14:28:17 -0500

    App.js (project managing)

--- ratingapp/src/App.js
+++ ratingapp/src/App.js
@@ -2,15 +2,15 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React from "react";
 
 import './App.css';
+import HomeView from './views/homeview';
+import CreateView from './views/createview';
+import ReadView from './views/readview';
+import UpdateView from './views/updateview';
+import DeleteView from './views/deleteview';
+import LoginView from './views/loginview';
+import RegisterView from './views/registerview';
 
-import HomeV from './components/homeview';
-import CreateV from './views/createview';
-import ReadV from './views/readview';
-import UpdateV from './views/updateview';
-import DeleteV from './views/deleteview';
-import LoginV from './views/loginview';
-import RegisterV from './views/registerview';
-
+//<Route path="/delete/:id" element={<UpdateView onClose={() =>{}} />} />
 
 
 
@@ -20,15 +20,17 @@ function App() {
     // setting different routing parth for different files 
     <BrowserRouter>
         <Routes>
-              <Route path="/" element={<HomeV />} />
-              <Route path="/register" element={<RegisterV />} />
-              <Route path="/login" element={<LoginV />} />
-              <Route path="/create" element={<CreateV />} />
-              <Route path="/read" element={<ReadV />} />
-              <Route path="/update/:id" element={<UpdateV onClose={() =>{}} />} />
-              <Route path="/delete" element={<DeleteV />} />
+              <Route path="/" element={<HomeView />} />
+              <Route path="/create" element={<CreateView />} />
+              <Route path="/read" element={<ReadView />} />
+              <Route path="/update/:id" element={<UpdateView onClose={() =>{}} />} />
+              <Route path="/delete/:id" element={<DeleteView onClose={() =>{}} />} />
+              <Route path="/register" element={<RegisterView />} />
+              <Route path="/login" element={<LoginView />} />
         </Routes>
     </BrowserRouter>
   );
 }
+
 export default App;
+
